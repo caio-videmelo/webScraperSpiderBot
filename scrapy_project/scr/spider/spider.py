b80846cf4162e625426ef39536bd8d0768e86232
Caio Melo <caio.videmelo@gmail.com>
2024-09-12 11:15:27 -0300

    Update spider.py

--- scrapy_project/scr/spider/spider.py
+++ scrapy_project/scr/spider/spider.py
@@ -1,39 +1,46 @@
-import scrapy
-import csv
 from time import sleep
+import scrapy
+from scrapy.exporters import CsvItemExporter
 
 class GoodReadsSpider(scrapy.Spider):
     """Spider to scrape quotes from Goodreads."""
     
-    name = 'quotebot2'
+    name = 'quotebot'
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the spider and set up the CSV exporter."""
+        super(GoodReadsSpider, self).__init__(*args, **kwargs)
+        # Open the CSV file in binary write mode
+        self.file = open('quotes_data.csv', 'wb')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    def close(self, reason):
+        """Finish exporting and close the CSV file."""
+        self.exporter.finish_exporting()
+        self.file.close()
 
     def start_requests(self):
         """Initialize requests to scrape the specified URLs."""
+        # Define the URL(s) to scrape
         urls = ['https://www.goodreads.com/quotes?page=1']
-        
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        """Parse the response and extract quote data."""
+        """Process the returned response and extract quote data."""
         for element in response.xpath("//div[@class='quote']"):
-            yield {
+            item = {
                 'quote': element.xpath(".//div[@class='quoteText']/text()").get(),
                 'author': element.xpath(".//span[@class='authorOrTitle']/text()").get(),
                 'tags': element.xpath(".//div[@class='greyText smallText left']/a/text()").getall(),
             }
+            # Export the item using the exporter
+            self.exporter.export_item(item)
+            yield item  # Yield the item for Scrapy's pipeline
 
+        # Find the link to the next page and extract the page number
         next_page_number = response.xpath("//a[@class='next_page']/@href").get()
-        if next_page_number:
-            next_page_url = f'https://www.goodreads.com/quotes?page={next_page_number.split("=")[1]}'
-            yield scrapy.Request(url=next_page_url, callback=self.parse)
-
-    def close(self, reason):
-        """Export scraped data to a CSV file upon closing the spider."""
-        with open('quotes.csv', 'w', newline='', encoding='utf-8') as csvfile:
-            fieldnames = ['quote', 'author', 'tags']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-            writer.writeheader()
-            for item in self.crawler.stats.get_value('item_scraped_count'):
-                writer.writerow(item)
+        if next_page_number is not None:
+            next_page_link = f'https://www.goodreads.com{next_page_number}'
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
