180fdd778648adb5fb01097f6ea23c767180523c
Caio Melo <caio.videmelo@gmail.com>
2024-09-12 11:37:08 -0300

    Update spider.py

--- scrapy_project/scr/spider/spider.py
+++ scrapy_project/scr/spider/spider.py
@@ -1,6 +1,7 @@
-from time import sleep
 import scrapy
-from scrapy.exporters import CsvItemExporter
+from scrapy.exporters import CsvItemExporter, XmlItemExporter, JsonItemExporter
+from scrapy.loader import ItemLoader
+
 
 class GoodReadsSpider(scrapy.Spider):
     """Spider to scrape quotes from Goodreads."""
@@ -8,17 +9,31 @@ class GoodReadsSpider(scrapy.Spider):
     name = 'quotebot'
 
     def __init__(self, *args, **kwargs):
-        """Initialize the spider and set up the CSV exporter."""
+        """Initialize the spider and set up the exporters."""
         super(GoodReadsSpider, self).__init__(*args, **kwargs)
-        # Open the CSV file in binary write mode
-        self.file = open('quotes_data.csv', 'wb')
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.start_exporting()
+        # Open files for exporting data
+        self.csv_file = open('quotes_data.csv', 'wb')
+        self.xml_file = open('quotes_data.xml', 'wb')
+        self.json_file = open('quotes_data.json', 'wb')
+
+        # Set up exporters
+        self.csv_exporter = CsvItemExporter(self.csv_file)
+        self.xml_exporter = XmlItemExporter(self.xml_file)
+        self.json_exporter = JsonItemExporter(self.json_file)
+
+        # Start exporting
+        self.csv_exporter.start_exporting()
+        self.xml_exporter.start_exporting()
+        self.json_exporter.start_exporting()
 
     def close(self, reason):
-        """Finish exporting and close the CSV file."""
-        self.exporter.finish_exporting()
-        self.file.close()
+        """Finish exporting and close the files."""
+        self.csv_exporter.finish_exporting()
+        self.xml_exporter.finish_exporting()
+        self.json_exporter.finish_exporting()
+        self.csv_file.close()
+        self.xml_file.close()
+        self.json_file.close()
 
     def start_requests(self):
         """Initialize requests to scrape the specified URLs."""
@@ -35,8 +50,12 @@ class GoodReadsSpider(scrapy.Spider):
                 'author': element.xpath(".//span[@class='authorOrTitle']/text()").get(),
                 'tags': element.xpath(".//div[@class='greyText smallText left']/a/text()").getall(),
             }
-            # Export the item using the exporter
-            self.exporter.export_item(item)
+
+            # Export the item using the exporters
+            self.csv_exporter.export_item(item)
+            self.xml_exporter.export_item(item)
+            self.json_exporter.export_item(item)
+
             yield item  # Yield the item for Scrapy's pipeline
 
         # Find the link to the next page and extract the page number
